33da5d88d16515c83a1951d9f7c772ba39bf2dea
Yan Pilon <y.pilon9@gmail.com>
2024-01-27 14:01:07 -0500

    configured default GET endpoint

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,25 @@
 import express from "express";
 import axios from "axios"
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import { uuid } from 'uuidv4';
 
 const app = express();
 const port = 3000;
 const database = 'mongodb://127.0.0.1:27017/WikiFly'
 
-mongoose.connect(database);
+const wikiAPI = axios.create({
+    baseURL: 'https://en.wikipedia.org/api/rest_v1/page/summary/',
+    headers: {}
+  });
+
+const chatgptAPI = axios.create({
+    baseURL: 'https://some-domain.com/api/',
+    timeout: 1000,
+    headers: {}
+  });
+
+//mongoose.connect(database);
 
 // const usersSchema = new mongoose.Schema({
 //     uname: String,
@@ -19,8 +32,10 @@ mongoose.connect(database);
 //   usersSchema
 // );
 
-app.get("/", (req, res) => {
-    
+app.get("/:wordQ", async (req, res) => {
+    let wordQ = req.params.wordQ;
+    let resData = await wikiAPI.get(`/${wordQ}`);
+    res.send(resData.data.extract);
 })
 
 app.listen(port, ()=>{
