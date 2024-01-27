8c8dfc2c3367f58ddcef05e2d8195b52c36afd58
Yan Pilon <y.pilon9@gmail.com>
2024-01-27 15:18:46 -0500

    very user has auth token and create one if necessary

--- Backend/app.js
+++ Backend/app.js
@@ -19,23 +19,37 @@ const chatgptAPI = axios.create({
     headers: {}
   });
 
-//mongoose.connect(database);
+await mongoose.connect(database);
 
-// const usersSchema = new mongoose.Schema({
-//     uname: String,
-//     password: String,
-//     email: String
-// });
+const userSchema = new mongoose.Schema({
+    token: String,
+    workQs: [{ id : Number, word: String}]
+});
 
-// const User = mongoose.model(
-//   "User",
-//   usersSchema
-// );
+const User = mongoose.model(
+  "User",
+  userSchema
+);
 
 app.get("/:wordQ", async (req, res) => {
     let wordQ = req.params.wordQ;
     let resData = await wikiAPI.get(`/${wordQ}`);
     res.send(resData.data.extract);
+})
+
+app.get("/auth/tokenReq", async (req, res) => {
+
+    while(true){
+        //create new token 
+        const newToken = uuid();
+        //check for uniqueness in database
+        const balls = await User.findOne({ token: newToken});
+        //if unique send back to user & create user entry
+        if(balls === null){
+            res.send(newToken);
+            break;
+        }
+    }
 })
 
 app.listen(port, ()=>{
--- Extension/background.js
+++ Extension/background.js
@@ -1,9 +1,48 @@
 const serverAdd = 'http://localhost:3000/'
 
+// chrome.storage.local.set({ key: value }).then(() => {
+//     console.log("Value is set");
+// });
+
+// chrome.storage.local.get(["key"]).then((result) => {
+//     console.log("Value is " + result.key);
+// });
+
 function queryWord(){
 
 }
 
 function queryArticle(){
     
+}
+
+
+function auth() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(["wikiAuthKey"], (result) => {
+            if (result.wikiAuthKey !== undefined) {
+                resolve(result.wikiAuthKey);
+            } else {
+                fetch(serverAdd.concat('auth/tokenReq'))
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('HTTP request failed with status:', response.status);
+                        }
+                        return response.json();
+                    })
+                    .then(data => {
+                        console.log(data);
+                        const value = data.authToken; 
+                        chrome.storage.local.set({ wikiAuthKey: value }, () => {
+                            console.log("Value is set");
+                            resolve(value);
+                        });
+                    })
+                    .catch(error => {
+                        console.error('Error:', error.message);
+                        reject(error);
+                    });
+            }
+        });
+    });
 }
